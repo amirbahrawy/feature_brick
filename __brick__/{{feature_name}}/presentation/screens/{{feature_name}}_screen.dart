0d8115653bcba09d2122dce9b0b6836597f4e502
amirbahrawy <38887148+amirbahrawy@users.noreply.github.com>
2025-05-26 15:01:55 +0300

    Update {{feature_name}}_screen.dart

--- __brick__/{{feature_name}}/presentation/screens/{{feature_name}}_screen.dart
+++ __brick__/{{feature_name}}/presentation/screens/{{feature_name}}_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:trivioo_mobile_app/core/shared/shared_widgets/general_purpose/app_loading.dart';
+import '/core/shared/shared_widgets/general_purpose/app_loading.dart';
 import '../cubit/{{feature_name}}_cubit.dart';
 
 class {{feature_name.pascalCase()}}Screen extends StatelessWidget {
@@ -9,7 +9,7 @@ class {{feature_name.pascalCase()}}Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (_) => getIt<{{feature_name.pascalCase()}}Cubit>()..init()
+      create: (_) => getIt<{{feature_name.pascalCase()}}Cubit>()..init(),
       child: Scaffold(
         appBar: AppBar(
           title: Text('{{feature_name.titleCase()}} Screen'),
@@ -17,7 +17,7 @@ class {{feature_name.pascalCase()}}Screen extends StatelessWidget {
         body: BlocBuilder<{{feature_name.pascalCase()}}Cubit, {{feature_name.pascalCase()}}State>(
           builder: (context, state) {
             if (state.isLoading) {
-              return Center(child: AppLoading());
+              return const Center(child: AppLoading());
             }
             return Container();
           },
